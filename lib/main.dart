8dad69313e59e1ae883fa8b57151c68b5bd1557e
guitarmanjon <guitarmanjon@yahoo.com>
2021-01-25 21:01:22 +0000

    20210125 2101 playing with text boxes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blue,
+        body: Center(
+            child: Column(
+          children: [
+            Text('Hello', textAlign: TextAlign.center),
+            Text('My name is...', textAlign: TextAlign.center),
+            Text('data')
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
